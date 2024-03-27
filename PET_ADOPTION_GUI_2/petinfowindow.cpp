6cdc0f05797ea7a40fc7fb10005ca9055c7f31c0
Osezuah <122998658+Osezuah@users.noreply.github.com>
2024-03-26 20:05:40 -0400

    Accomplished collecting animal type info from GUI

--- PET_ADOPTION_GUI_2/petinfowindow.cpp
+++ PET_ADOPTION_GUI_2/petinfowindow.cpp
@@ -1,20 +1,23 @@
 #include "petinfowindow.h"
 #include "ui_petinfowindow.h"
+//#include "Packet.h"
+#include <iostream>
 #include <QButtonGroup>
-
+#include <QAbstractButton>
+using namespace std;
 PetInfoWindow::PetInfoWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::PetInfoWindow)
 {
     ui->setupUi(this);
     // Create a button group for the animal type radio buttons
-    QButtonGroup *animalGroup = new QButtonGroup(this);
+    animalGroup = new QButtonGroup(this);
     animalGroup->addButton(ui->radioDogs);
     animalGroup->addButton(ui->radioCats);
     animalGroup->addButton(ui->radioBoth);
 
     // Create a button group for the vaccination status radio buttons
-    QButtonGroup *vaccinationGroup = new QButtonGroup(this);
+    vaccinationGroup = new QButtonGroup(this);
     vaccinationGroup->addButton(ui->radioYes);
     vaccinationGroup->addButton(ui->radioNo);
     vaccinationGroup->addButton(ui->radioSome);
@@ -22,6 +25,8 @@ PetInfoWindow::PetInfoWindow(QWidget *parent)
     pushButtonCancel = findChild<QPushButton*>("pushButtonCancel");
     connect(pushButtonCancel, &QPushButton::clicked, this, &PetInfoWindow::cancelClicked);
 
+    submitButton = findChild<QPushButton*>("pushButtonSubmit");
+    connect(submitButton, &QPushButton::clicked, this, &PetInfoWindow::petInfoSubmitButtonClicked);
 }
 
 void PetInfoWindow::cancelClicked()
@@ -33,4 +38,30 @@ void PetInfoWindow::cancelClicked()
 PetInfoWindow::~PetInfoWindow()
 {
     delete ui;
+}
+
+void PetInfoWindow::petInfoSubmitButtonClicked()
+{
+    QAbstractButton *animalButton = animalGroup->checkedButton();
+
+    //collects for animal type info
+    if (animalButton == ui->radioDogs)
+    {
+        // Packet pkt;
+        // pkt.petInfo.animalType = 0;
+        cout << "Dog is 0" << endl;
+    }
+    else if (animalButton == ui->radioCats)
+    {
+        // Packet pkt;
+        // pkt.petInfo.animalType = 1;
+        cout << "Cat is 1" << endl;
+    }
+    else if (animalButton == ui->radioBoth)
+    {
+        // Packet pkt;
+        // pkt.petInfo.animalType = 2;
+        cout << "Both is 2" << endl;
+    }
+    this->close();
 }
