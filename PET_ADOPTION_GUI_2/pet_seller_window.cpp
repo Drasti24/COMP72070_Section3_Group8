633de03675914d435ecb0691210039bf95bfef2c
Drasti24 <113472854+Drasti24@users.noreply.github.com>
2024-03-27 20:24:18 -0400

    Updated pet_seller_window.cpp to get entered information

--- PET_ADOPTION_GUI_2/pet_seller_window.cpp
+++ PET_ADOPTION_GUI_2/pet_seller_window.cpp
@@ -1,5 +1,8 @@
 #include "pet_seller_window.h"
 #include "ui_pet_seller_window.h"
+#include "PetSeller.h"
+#include <iostream>
+using namespace std;
 
 PetSellerWindow::PetSellerWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -9,6 +12,9 @@ PetSellerWindow::PetSellerWindow(QWidget *parent)
 
     pushButtonCancel = findChild<QPushButton*>("pushButtonCancel");
     connect(pushButtonCancel, &QPushButton::clicked, this, &PetSellerWindow::cancelClicked);
+
+    submitButton = findChild<QPushButton*>("pushButtonSubmit");
+    connect(submitButton, &QPushButton::clicked, this, &PetSellerWindow::submitButtonClicked);
 }
 
 PetSellerWindow::~PetSellerWindow()
@@ -19,5 +25,31 @@ PetSellerWindow::~PetSellerWindow()
 void PetSellerWindow::cancelClicked()
 {
     // Implement cancel functionality here
-     this->close();
+    this->close();
+}
+
+void PetSellerWindow::submitButtonClicked()
+{
+    QString Fname = ui->lineEditFirstName->text();
+    QString Lname = ui->lineEditLastName->text();
+    QString Uname = ui->lineEditUsername->text();
+    QString Pword = ui->lineEditPassword->text();
+    QString BLoc =  ui->lineEditBusinessAddress->text();
+    QString Prvc =  ui->lineEditProvince->text();
+    QString PCode = ui->lineEditPostalCode->text();
+    QString PNum =  ui->lineEditPhoneNumber->text();
+
+    emit submitSellerData(Fname, Lname, Uname, Pword, BLoc, Prvc, PCode, PNum);
+
+    PetSeller myPS(Fname.toStdString(), Lname.toStdString(), Uname.toStdString(), Pword.toStdString(),
+                   BLoc.toStdString(), Prvc.toStdString(), PCode.toStdString(), PNum.toStdString());
+
+    cout << myPS.getFirstName() << endl;
+    cout << myPS.getLastName() << endl;
+    cout << myPS.getUsername() << endl;
+    cout << myPS.getPassword()  << endl;
+    cout << myPS.getBusinessLocation() << endl;
+    cout << myPS.getProvince() << endl;
+    cout << myPS.getPostalCode() << endl;
+    this->close();
 }
